2525a0d045bc1ecea9e20b2136405efc0c2bc069
Josiah Maina <josiah5115@gmail.com>
2018-06-25 12:34:40 -0600

    testing

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # git-lecture-wpr39-2
 Learning more about git
+
+Read the rest of this to be awesome.
+
+Another change.
---
+++ code.js
@@ -0,0 +1,4 @@
+var introduction = 'I am Joh and I love coding';
+
+var journal =['Today I got to start my coding class and I am so excited to learn Javascript'];
+
